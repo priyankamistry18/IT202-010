d496bd80de7e3977b235c1fc37e80a69c453a457
priyankamistry18 <Priyankamistry29@gmail.com>
2021-02-17 20:47:11 -0500

    flow control hw

---
+++ FlowControl/php_IfElse.php
@@ -0,0 +1,37 @@
+<?php
+echo "<br>\nPHP Exercise [IF...Else] Exercise [1]<br>\n";
+$a = 50;
+$b = 10;
+if($a > $b){
+  echo "Hello World";
+}
+echo "<br>\n";
+echo "<br>\nPHP Exercise [IF...Else] Exercise [2]<br>\n";
+$a = 50;
+$b = 10;
+if($a != $b) {
+  echo "Hello World";
+}
+echo "<br>\nPHP Exercise [IF...Else] Exercise [3]<br>\n";
+echo "<br>\n";
+$a = 50;
+$b = 10;
+if
+ ($a == $b) {
+  echo "Yes";
+} else {
+  echo "No";
+}
+echo "<br>\n";
+echo "<br>\nPHP Exercise [IF...Else] Exercise [4]<br>\n";
+$a = 50;
+$b = 10;
+if
+ ($a == $b) {
+  echo "1";
+} elseif ($a > $b) {
+  echo "2";
+} else {
+  echo "3";
+}
+?>
