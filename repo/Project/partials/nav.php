a40eb16edd966b14ba169e1920ba6f57c29b9c95
priyankamistry18 <60395460+priyankamistry18@users.noreply.github.com>
2021-05-07 23:53:46 -0400

    Update nav.php

--- repo/Project/partials/nav.php
+++ repo/Project/partials/nav.php
@@ -10,6 +10,12 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <li><a href="login.php">Login</a></li>
         <li><a href="register.php">Register</a></li>
     <?php endif; ?>
+ <?php if (has_role("Admin")): ?>
+            <li><a href="test_create_account.php">Create Account</a></li>
+            <li><a href="test_list_account.php">View Accounts</a></li>
+            <li><a href="test_create_transactions.php">Create transaction</a></li>
+            <li><a href="test_list_transactions.php">View transactions</a></li>
+    <?php endif; ?>
     <?php if (is_logged_in()): ?>
         <li><a href="profile.php">Profile</a></li>
         <li><a href="logout.php">Logout</a></li>
